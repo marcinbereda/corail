1e9d69e341de41199557e35d1ffd22e27b131ba7
Marcin Bereda <marcin@w3a.pl>
2015-06-27 22:52:14 +0200

    Inicjalizacja

---
+++ corail/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Corail - service',
+    'version' : '1.0',
+    'author' : 'Marcin Bereda',
+    'category' : '',
+    'description' : """
+    CORAIL Service
+    """,
+    'website': '',
+    'depends' : [],
+    'data': [
+    ],
+    'qweb' : [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
