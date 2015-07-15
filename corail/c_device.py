b25b780db3763058c91123a224efceece61bbd59
Marcin Bereda <marcin@w3a.pl>
2015-07-15 23:23:41 +0200

    Zmiany w formularzu urządzenia

--- corail/c_device.py
+++ corail/c_device.py
@@ -5,6 +5,7 @@ from openerp.exceptions import except_orm, Warning, RedirectWarning
 
 from dateutil.relativedelta import relativedelta
 import datetime
+import calendar
 import pdb
 
 Device_State = [
@@ -25,20 +26,94 @@ class c_device(models.Model):
         else:
             self.motohours = 0.0
             self.add_motohours = True
+            
+    @api.one
+    def _get_age_device(self):
+        date_str = {
+                    'year': ['rok', 'lata', 'lat'],
+                    'month': ['m-c', 'm-ce', 'm-cy'],
+                    'day': ['dzień', 'dni']
+                    }
+        
+        if self.date_delivery:
+            age = ''
+            today = datetime.date.today()
+            date_delivery = datetime.datetime.strptime(self.date_delivery, '%Y-%m-%d').date()
+
+            years = today.year - date_delivery.year
+            months = today.month - date_delivery.month
+            days = today.day - date_delivery.day
+            
+            if days < 0:
+                day_month = calendar.monthrange(date_delivery.year, date_delivery.month)[1]
+                days = day_month + days
+                months -= 1
+                
+            if months < 0:
+                months = 12 + months
+                years -= 1
+                
+            if years > 0:
+                if years >= 5:
+                    age += '%s %s '%(years, date_str['year'][2])
+                elif years >= 2:
+                    age += '%s %s '%(years, date_str['year'][1])
+                else:
+                    age += '%s %s '%(years, date_str['year'][0])
+                
+                if months > 0 and days > 0:
+                    age += ', '
+                elif months > 0 or days > 0:
+                    age += ' i '
+                    
+            if months > 0:
+                if months >= 5:
+                    age += '%s %s '%(months, date_str['month'][2])
+                elif months >= 2:
+                    age += '%s %s '%(months, date_str['month'][1])
+                else:
+                    age += '%s %s '%(months, date_str['month'][0])
+                    
+                if days > 0:
+                    age += ' i '
+                    
+            if days > 0:
+                if days >= 2:
+                    age += '%s %s '%(days, date_str['day'][1])
+                else:
+                    age += '%s %s '%(days, date_str['day'][0])
+            self.age_device = age
     
     name = fields.Many2one('c.device.model', string='Nazwa', required=True)
     partner_id = fields.Many2one('res.partner', string='Klient', domain=[('customer', '=', 'True'),('is_company','=',True)])
     serial_number = fields.Char(string='Numer seryjny')
     date_delivery = fields.Date(string='Data dostawy')
+    age_device = fields.Char(compute='_get_age_device', string='Wiek', store=False)
     warranty_start = fields.Date(string='Gwarancja od')
     warranty_stop = fields.Date(string='Gwaracncja do')
-    motohours = fields.Float(compute='_get_motohours', string='Przepracowane motogodziny', store=False)
+    motohours = fields.Float(compute='_get_motohours', string='Motogodziny', store=False)
     add_motohours = fields.Boolean(compute='_get_motohours', string='Dodanie motogodzin', store=False)
     rtime_id = fields.Many2one('c.device.response.time', string='Czas reakcji')
     #state = fields.Selection(Device_State, string='Jest używane', default='used')
     attachment_ids = fields.Many2many('ir.attachment', 'c_device_attachment_rel', 'c_device_templ_id', 'attachment_id', string='Załączniki')
     task_ids = fields.One2many('c.task', 'device_id', string='Zadania')
     motohours_ids = fields.One2many('c.device.motohours', 'device_id', string='Motogodziny')
+    image = fields.Binary(string='Zdjęcie')
+    description = fields.Text(string='Uwagi')
+    
+    category = fields.Char(string='Kategoria')
+    purpose = fields.Char(string='Przeznaczenie')
+    specifications = fields.Text(string='Charakretystyczne parametry techniczne')
+    permissions = fields.Text(string='Wymagane uprawnienia')
+    
+    tecnical_doc_data = fields.Binary(string='Dokumentacja techniczno-ruchowa')
+    tecnical_doc_name = fields.Char(string='Nazwa pliku')
+    user_manual_data = fields.Binary(string='Instrukcja użytkownika')
+    user_manual_name = fields.Char(string='Nazwa pliku')
+    maint_manual_data = fields.Binary(string='Instrukcja konserwacji')
+    maint_manual_name = fields.Char(string='Nazwa pliku')
+    spare_parts_data = fields.Binary(string='Lista części zamiennych')
+    spare_parts_name = fields.Char(string='Nazwa pliku')
     
 
 class c_device_model(models.Model):
