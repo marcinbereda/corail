881c09e031f6b457734b4b0818e74be7fa5cf42c
Marcin Bereda <marcin@w3a.pl>
2015-07-17 09:13:19 +0200

    Poprawki

--- corail/c_task.py
+++ corail/c_task.py
@@ -23,9 +23,14 @@ class c_task(models.Model):
     _inherit = ['mail.thread']
     
     name = fields.Char(string='Opis', required=True)
-    device_id = fields.Many2one('c.device', string='Urządzenie', required=True)
+    device_id = fields.Many2one('c.device', string='Urządzenie', required=True, domain="[('partner_id','=',partner_id)]")
+    serial_number = fields.Char(string='Numer seryjny', related='device_id.serial_number', readonly=True)
     partner_id = fields.Many2one('res.partner', string='Klient', domain=[('customer', '=', 'True'),('is_company','=',True)], required=True)
     date_start = fields.Datetime(string='Data rozpoczęcia', required=True)
     date_stop = fields.Datetime(string='Data zakończenia', required=True)
     state = fields.Selection(Task_State, string='Status', default='scheduled', required=True, readonly=True)
     performs = fields.Selection(Performs, string='Wykonuje', default='client', required=True)
+    description = fields.Text(string='Procedura szczegółowa')
+    
+    manual_data = fields.Binary(string='Procedura szczegółowa')
+    manual_name = fields.Char(string='Nazwa pliku')
